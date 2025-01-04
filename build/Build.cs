5e56239b5b4cbad9a8a0c4d181711e02b0f0e0bf
Максим Ерошкин <ema@rndc.pl>
2025-01-04 13:21:01 +0300

    Add Nuke.Build

---
+++ build/Build.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+
+class Build : NukeBuild
+{
+    public static int Main () => Execute<Build>(x => x.Run);
+
+    /// <summary>
+    /// online converter: https://dom111.github.io/image-to-ansi/
+    /// </summary>
+    Target Run => _ => _
+        .Executes(() =>
+        {
+            var logo = File.ReadAllText(RootDirectory / "snowman.txt");
+            logo = Regex.Replace(logo, @"(\r\n|\n)", Environment.NewLine);
+            Console.WriteLine(logo);
+        });
+
+}
