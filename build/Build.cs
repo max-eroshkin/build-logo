ecf93a00da74239cac8c2b8c68aa47a0df63f0d7
Максим Ерошкин <ema@rndc.pl>
2025-04-01 10:13:35 +0300

    clown

--- build/Build.cs
+++ build/Build.cs
@@ -23,7 +23,7 @@ class Build : NukeBuild
     Target Run => _ => _
         .Executes(() =>
         {
-            var logo = File.ReadAllText(RootDirectory / "buket.txt");
+            var logo = File.ReadAllText(RootDirectory / "clown.txt");
             logo = Regex.Replace(logo, @"(\r\n|\n)", Environment.NewLine);
             Console.WriteLine(logo);
         });
